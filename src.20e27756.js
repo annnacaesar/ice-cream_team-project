6f1325ee37d3fb80ed18288867ffc4d6968c163d
annnacaesar <92438869+annnacaesar@users.noreply.github.com>
2022-01-14 15:57:32 +0000

    Deploying to gh-pages from @ annnacaesar/team-project7@33f6c2c80ba367a48a4cc83a9fdd9a597de76ef1 🚀

--- src.20e27756.js
+++ src.20e27756.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project7/src.fc2a46cf.js.map
+//# sourceMappingURL=/team-project7/src.20e27756.js.map
