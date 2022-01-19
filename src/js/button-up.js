7082637df879f00b6df05cd33d8b55dd6fcc079d
annnacaesar <Anechka22334@gmail.com>
2022-01-19 08:54:57 +0200

    id

--- src/js/button-up.js
+++ src/js/button-up.js
@@ -25,3 +25,18 @@
   window.addEventListener('scroll', trackScroll);
   goTopBtn.addEventListener('click', backToTop);
 })();
+
+jQuery(document).ready(function () {
+  var btn = $('#button');
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+  btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+  });
+});
